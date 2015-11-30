cd11559eec57894e0156281ba196e3acc7979860
Matthew Thomas <matthew.e.thomas@outlook.com>
2015-11-29 23:44:02 -0600

    Simplified code

--- Source/DesignTimeRegions/DesignTimeViewProviderBase.cs
+++ Source/DesignTimeRegions/DesignTimeViewProviderBase.cs
@@ -4,6 +4,9 @@ using System.Windows.Controls;
 
 namespace DesignTimeRegions
 {
+    /// <summary>
+    /// Base implementation for design time view providers
+    /// </summary>
     public abstract class DesignTimeViewProviderBase
     {        
         private readonly IDictionary<string, Type> ViewRegistrations = new Dictionary<string, Type>();
@@ -12,7 +15,7 @@ namespace DesignTimeRegions
 
         public void Initialize()
         {
-            if(IsInitialized)
+            if (IsInitialized)
             { return; }
 
             RegisterViewsWithContainer();
@@ -20,15 +23,25 @@ namespace DesignTimeRegions
             IsInitialized = true;
         }
 
+        /// <summary>
+        /// Registers the views and any dependent design time view models with their appropriate containers
+        /// </summary>
         protected abstract void RegisterViewsWithContainer();
 
+        /// <summary>
+        /// Registers the type of view with the appropriate named region. Use the <see cref="RegisterViewWithRegion{T}(string)"/> helper method in implementing this function.
+        /// </summary>
         protected abstract void RegisterViewsWithRegions();
 
+        /// <summary>
+        /// Resolves the desired view from the container and provides an instance of the desired view.
+        /// </summary>
+        /// <param name="viewType">The desired type of view</param>        
         protected abstract object ResolveView(Type viewType);
 
         protected void RegisterViewWithRegion<T>(string regionName)
         {
-            if(ViewRegistrations.ContainsKey(regionName) == false)
+            if (ViewRegistrations.ContainsKey(regionName) == false)
             {
                 ViewRegistrations.Add(regionName, typeof(T));
             }
@@ -39,12 +52,13 @@ namespace DesignTimeRegions
             object view = null;
             Type viewType;
 
-            if(ViewRegistrations.TryGetValue(regionName, out viewType))
+            if (ViewRegistrations.TryGetValue(regionName, out viewType))
             {
                 try
                 {
                     view = ResolveView(viewType);
-                }catch(Exception ex)
+                }
+                catch (Exception ex)
                 {
                     view = new TextBlock() { Text = ex.Message };
                 }
@@ -56,6 +70,5 @@ namespace DesignTimeRegions
 
             return view;
         }
-
     }
 }
--- Source/DesignTimeRegions/RegionProvider.cs
+++ Source/DesignTimeRegions/RegionProvider.cs
@@ -1,10 +1,7 @@
 ﻿using Prism.Regions;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
@@ -12,56 +9,32 @@ using System.Windows.Controls.Primitives;
 namespace DesignTimeRegions
 {
     public class RegionProvider
-    {
-
-        public static Type GetDesignTimeViewProviderType(DependencyObject obj)
+    {      
+        public static DesignTimeViewProviderBase GetViewProvider(DependencyObject obj)
         {
-            return (Type)obj.GetValue(DesignTimeViewProviderTypeProperty);
+            return (DesignTimeViewProviderBase)obj.GetValue(ViewProviderProperty);
         }
 
-        public static void SetDesignTimeViewProviderType(DependencyObject obj, Type value)
+        public static void SetViewProvider(DependencyObject obj, DesignTimeViewProviderBase value)
         {
-            obj.SetValue(DesignTimeViewProviderTypeProperty, value);
+            obj.SetValue(ViewProviderProperty, value);
         }
+      
+        public static readonly DependencyProperty ViewProviderProperty =
+            DependencyProperty.RegisterAttached("ViewProvider", typeof(DesignTimeViewProviderBase), typeof(RegionProvider), new PropertyMetadata(null, ViewProvider_Changed));
 
-        // Using a DependencyProperty as the backing store for DesignTimeViewProviderType.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty DesignTimeViewProviderTypeProperty =
-            DependencyProperty.RegisterAttached("DesignTimeViewProviderType", typeof(Type), typeof(RegionProvider), new PropertyMetadata(null));
-
-
-
-
-
-        public static string GetRegionName(DependencyObject obj)
+        private static void ViewProvider_Changed(DependencyObject sender, DependencyPropertyChangedEventArgs e)
         {
-            return (string)obj.GetValue(RegionNameProperty);
-        }
-
-        public static void SetRegionName(DependencyObject obj, string value)
-        {
-            obj.SetValue(RegionNameProperty, value);
-        }
-
-        // Using a DependencyProperty as the backing store for RegionName.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty RegionNameProperty =
-            DependencyProperty.RegisterAttached("RegionName", typeof(string), typeof(RegionProvider), new PropertyMetadata(null, RegionName_Changed));
-
-
-        private static void RegionName_Changed(DependencyObject sender, DependencyPropertyChangedEventArgs e)
-        {
-            var regionName = e.NewValue.ToString();
-            RegionManager.SetRegionName(sender, regionName);
-
             if (DesignerProperties.GetIsInDesignMode(sender) == false)
-            { return; } //we're in run-time, so the rest is not necessary            
-            
+            { return; } //we're in run-time, so the rest is not necessary  
+
+            var viewProvider = e.NewValue as DesignTimeViewProviderBase;
+            var regionName = RegionManager.GetRegionName(sender);
+
             object designTimeView = null;
             try
             {
-                var viewProviderType = GetDesignTimeViewProviderType(sender);
-                var viewProvider = (DesignTimeViewProviderBase)Activator.CreateInstance(viewProviderType);
                 viewProvider.Initialize();
-
                 designTimeView = viewProvider.GetViewForRegion(regionName);
             }
             catch (Exception ex)
@@ -74,17 +47,16 @@ namespace DesignTimeRegions
             {
                 (sender as ContentControl).Content = designTimeView;
             }
-            else if(sender is Selector)
+            else if (sender is Selector)
             {
                 var itemsSource = new object[] { designTimeView };
                 (sender as Selector).ItemsSource = itemsSource;
                 (sender as Selector).SelectedItem = itemsSource.First();
             }
-            else if(sender is ItemsControl)
+            else if (sender is ItemsControl)
             {
                 (sender as ItemsControl).ItemsSource = new object[] { designTimeView };
-            }            
+            }
         }
-
     }
 }
